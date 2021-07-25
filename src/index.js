5f45f2530ae2cf5c975ac086eeae37874724bb94
Chris Kendig <ckendig@spigot.com>
2021-07-25 08:25:47 -0400

    added cors dependency to allow cross origin requests

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 require('dotenv').config({path: '../.env'});
+const cors = require('cors');
 const app = express();
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cors({origin: true}));
 
 module.exports = app;
