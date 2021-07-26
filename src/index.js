0ba81b8d07c83689024a0bb93202d65ae227297a
Chris Kendig <ckendig@spigot.com>
2021-07-26 09:35:17 -0400

    added app.configure to index

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ const express = require('express');
 require('dotenv').config({path: '../.env'});
 const cors = require('cors');
 const app = express();
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(cors({origin: true}));
+app.configure(function(){
+    app.use(express.urlencoded({ extended: true }));
+    app.use(express.json());
+    app.use(cors({origin: true}));
+});
 
 module.exports = app;
