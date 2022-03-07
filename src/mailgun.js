cddbd0d90a6abc308ffe251f1d97242c8d42ec45
Chris Kendig <712interactive@gmail.com>
2022-03-06 21:13:56 -0500

    added principal to email list for open enrollment

--- src/mailgun.js
+++ src/mailgun.js
@@ -23,7 +23,7 @@ const sendContactEmailConfirmationToClient = (details) => {
 const sendOpenEnrollmentEmailToAdmin = (details) => {
     return {
         from: `${details.contact.firstName} ${details.contact.lastName} <${details.contact.email}>`,
-        to: process.env.CP_ADMIN_EMAIL,
+        to: `${process.env.CP_REGISTRAR_EMAIL}, ${process.env.CP_PRINCIPAL_EMAIL}`,
         subject: "Open Enrollment Inquiry - Crosspointe Academy",
         html: `<!DOCTYPE html><body><div style="width: 100%; padding: 10px; box-sizing: border-box;display: flex; justify-content: space-between; align-items: center"><img src='https://crosspointeacademyfortmyers.com/images/logo.png' alt="School Logo" style="max-width: 100px" /><h2>New Contact Inquiry Received</h2></div><table style="max-width: 550px; display: block;margin: 30px 0;"><tbody><tr><td style="font-weight: 700;">Name: </td><td>${details.contact.firstName} ${details.contact.lastName}</td></tr><tr><td style="font-weight: 700;">Phone: </td><td>${details.contact.phone}</td></tr><tr><td style="font-weight: 700;">Email: </td><td>${details.contact.email}</td></tr><tr><td style="font-weight: 700;">Inquiry: </td><td>${details.enrolling}</td></tr></tbody></table></body></html>`
     };
@@ -31,7 +31,7 @@ const sendOpenEnrollmentEmailToAdmin = (details) => {
 
 const sendOpenEnrollmentEmailConfirmationToClient = (details) => {
     return {
-        from: `Crosspointe Academy Front Office <${process.env.CP_ADMIN_EMAIL}>`,
+        from: `Crosspointe Academy Front Office <${process.env.CP_REGISTRAR_EMAIL}>`,
         to: details.contact.email,
         subject: "Open Enrollment Inquiry Received by Crosspointe Academy",
         html: `<!DOCTYPE html><body><div style="width: 100%; padding: 10px; box-sizing: border-box;display: flex; justify-content: space-between; align-items: center"><img src='https://crosspointeacademyfortmyers.com/images/logo.png' alt="School Logo" style="max-width: 100px" /><h2 style="flex-grow: 1; word-wrap: break-word;">Your Message Has Been Received</h2></div><div><p style="font-size: 17px;">Dear ${details.contact.firstName},</p><p style="font-size: 16px;">Thank you for contacting Crosspointe Academy.  We have received your message regarding Open Enrollment and will follow up with you in a timely manner. ${officeHours}.  We look forward to speaking with you soon.</p><p style="font-weight: 700; font-size: 17px;">Sincerely,</p><p>Crosspointe Academy Staff</p></div></body></html>`
